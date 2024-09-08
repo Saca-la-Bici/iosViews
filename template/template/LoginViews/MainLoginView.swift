6227ba435990e27c1534d3d684bef5abf6509250
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-09-08 13:22:26 -0600

    fix(UI): Add Apple ID sign in and fix style

--- template/template/LoginViews/MainLoginView.swift
+++ template/template/LoginViews/MainLoginView.swift
@@ -21,95 +21,126 @@ struct MainLoginView: View {
                         .resizable()
                         .frame(width: 188.0, height: 147.0)
                     
-                    Spacer().frame(height: 50)
+                    Spacer().frame(height: 30)
                     
                     // Form
-                    VStack(alignment: .center) {
+                    VStack(alignment: .center, spacing: 20) {
                         
-                        Text("¡Explora y disfruta Querétaro!")
-                            .font(.headline)
-                            .bold()
-                        
-                        Spacer().frame(height: 10)
-                        
-                        Text("Es genial tenerte aquí.")
-                            .font(.body)
-                        
-                        Spacer().frame(height: 20)
-                        
-                        // Sign in
-                        Button {
-                            // Code
-                        } label: {
-                            Text("Crea una cuenta")
+                        VStack(alignment: .center, spacing: 10) {
+                            Text("¡Explora y disfruta Querétaro!")
                                 .font(.headline)
                                 .bold()
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .foregroundColor(.white)
-                                .background(Color(red: 0.961, green: 0.802, blue: 0.048))
-                                .cornerRadius(10)
-                        }
-                        
-                        // Log in
-                        Button {
-                            // Code
-                        } label: {
-                            Text("Iniciar sesión")
+                            
+                            Text("Es genial tenerte aquí.")
                                 .font(.subheadline)
-                                .frame(maxWidth: .infinity)
-                                .padding()
-                                .foregroundColor(.black)
-                                .overlay (
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color(red: 0.851, green: 0.851, blue: 0.851), lineWidth: 2)
-                                )
-                        }
-                        
-                        Text("o continúa con")
-                            .font(.footnote)
-                            .padding(.vertical)
-                        
-                        Button(action: {
-                            // Acción de registro con Google
-                        }) {
-                            HStack {
-                                Image("GoogleLogo")
-                                    .resizable()
-                                    .frame(width: 20.0, height: 20.0)
-                                Text("Registrarse con Google")
-                                    .font(.subheadline)
-                                    .foregroundColor(.black)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(10)
-                        }
-                        
-                        Button(action: {
-                            // Acción de registro con Facebook
-                        }) {
-                            HStack {
-                                Image("FacebookLogo")
-                                    .resizable()
-                                    .frame(width: 20.0, height: 20.0)
-                                Text("Registrarse con Facebook")
-                                    .font(.subheadline)
-                                    .foregroundColor(.black)
-                            }
-                            .frame(maxWidth: .infinity)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(10)
+                                .fontWeight(.light)
                             
                         }
+                        
+                        VStack {
+                            // Sign in
+                            Button {
+                                // Code
+                            } label: {
+                                Text("Crea una cuenta")
+                                    .font(.headline)
+                                    .bold()
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .foregroundColor(.white)
+                                    .background(Color(red: 0.961, green: 0.802, blue: 0.048))
+                                    .cornerRadius(10)
+                            }
+                            
+                            // Log in
+                            Button {
+                                // Code
+                            } label: {
+                                Text("Iniciar sesión")
+                                    .font(.subheadline)
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                    .foregroundColor(.black)
+                                    .overlay (
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color(red: 0.851, green: 0.851, blue: 0.851), lineWidth: 2)
+                                    )
+                            }
+                        }
+                        
+                        Spacer().frame(height: 0)
+                        
+                        // O continúa con
+                        Text("o continúa con")
+                            .font(.footnote)
+                            .foregroundColor(Color.gray)
+                            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                        
+                        VStack {
+                            Button(action: {
+                                // Acción de autenticación con Google
+                            }) {
+                                HStack(alignment: .center, spacing: 15.0) {
+                                    Image("GoogleLogo")
+                                        .resizable()
+                                        .frame(width: 20.0, height: 20.0)
+                                    Text("Continuar con Google")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                            }
+                            
+                            Button(action: {
+                                // Acción de autenticación con Facebook
+                            }) {
+                                HStack(alignment: .center, spacing: 15.0) {
+                                    Image("FacebookLogo")
+                                        .resizable()
+                                        .frame(width: 20.0, height: 20.0)
+                                    Text("Continuar con Facebook")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                                
+                            }
+                            
+                            Button(action: {
+                                // Acción de autenticación con Apple
+                            }) {
+                                HStack(alignment: .center, spacing: 15.0) {
+                                    Image(systemName: "apple.logo")
+                                        .resizable()
+                                        .frame(width: 20.0, height: 25.0)
+                                        .foregroundColor(.black)
+                                    Text("Continuar con Apple ID")
+                                        .font(.subheadline)
+                                        .foregroundColor(.black)
+                                }
+                                .frame(maxWidth: .infinity)
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                            }
+                        }
+                        
+                        Spacer().frame(height: 10)
                         
                         
                     }.padding(.horizontal, 30.0)
                         .padding(.vertical, 20.0)
                         .background(.white)
-                        .cornerRadius(10)
+                        .cornerRadius(25)
+                    
+                    Spacer()
+                    
                 }.padding(.horizontal, 20)
                 
             }.zIndex(2)
